70f8e1825089638f570e7e0c48b0bbd3213d09cb
Skobcow Roman <djromeo.91@mail.ru>
2015-05-05 00:08:38 +0300

    start
    start

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $(".dropdown dt a").click(function() {
+        $(".dropdown dd ul").toggle();
+    });
+
+    $(".dropdown dd ul li a").click(function() {
+        var text = $(this).html();
+        $(".dropdown dt a span").html(text);
+        $(".dropdown dd ul").hide();
+    });
+
+    $(document).bind('click', function(e) {
+        var $clicked = $(e.target);
+        if (! $clicked.parents().hasClass("dropdown"))
+            $(".dropdown dd ul").hide();
+    });
+
+});
+
+$(document).ready(function(){
+    $('a[href*=#]').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 500);
+        e.preventDefault();
+    });
+    return false;
+});
